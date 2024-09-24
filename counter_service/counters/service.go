08fe2066a4d21749b95e26a5f00b2e125b6b7400
AlexeySigida <alexey.sigida@gmail.com>
2024-09-24 20:14:44 +0300

    add counter service

---
+++ counter_service/counters/service.go
@@ -0,0 +1,54 @@
+package counters
+
+import (
+	"counter-service/cache"
+	"counter-service/storage"
+	"time"
+)
+
+type CounterService struct {
+	cache   *cache.RedisCache
+	storage *storage.DB
+}
+
+func NewCounterService(cache *cache.RedisCache, storage *storage.DB) *CounterService {
+	return &CounterService{
+		cache:   cache,
+		storage: storage,
+	}
+}
+
+func (s *CounterService) GetUnreadCount(userID string) (int64, error) {
+	// Try to get counter from Redis
+	count, err := s.cache.Get(userID)
+	if err == nil {
+		return count, nil
+	}
+
+	// If not in Redis, fall back to DB
+	count, err = s.storage.GetCounter(userID)
+	if err != nil {
+		return 0, err
+	}
+
+	// Cache the counter value in Redis
+	_ = s.cache.Set(userID, count, 5*time.Minute)
+	return count, nil
+}
+
+func (s *CounterService) IncrementUnreadCount(userID string, increment int64) error {
+	// Increment in Redis
+	err := s.cache.IncrBy(userID, increment)
+	if err != nil {
+		return err
+	}
+
+	// Get updated counter value and persist in the DB
+	newCount, err := s.cache.Get(userID)
+	if err != nil {
+		return err
+	}
+
+	// Persist new value to DB
+	return s.storage.UpdateCounter(userID, newCount)
+}
